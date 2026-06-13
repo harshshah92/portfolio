07c779180aaa6493a474845aad8e17311b6e89fe
harsh-9592 <shahharsh9592@gmail.com>
2026-06-14 01:20:38 +0530

    Configure EmailJS credentials

--- harsh_portfolio/lib/config/emailjs_config.dart
+++ harsh_portfolio/lib/config/emailjs_config.dart
@@ -1,6 +1,6 @@
 class EmailJsConfig {
-  static const String serviceId = 'service_portfolio';
-  static const String templateId = 'template_contact';
-  static const String publicKey = 'YOUR_PUBLIC_KEY';
+  static const String serviceId = 'service_k9gx7a9';
+  static const String templateId = 'template_m5k8omn';
+  static const String publicKey = '7hX-jWGgXGVrRlVB0';
   static const String apiUrl = 'https://api.emailjs.com/api/v1.0/email/send';
 }
